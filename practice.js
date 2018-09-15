881a50dbc796e17f6c28c9ec9c5d585cdd992956
Enrique1990 <ecarrillo@techlaunch.io>
2018-09-15 17:28:59 -0400

    first commit

---
+++ practice.js
@@ -0,0 +1,68 @@
+console.log("testing...")
+
+
+// Exercise 1
+var oper1 = 12+12;
+console.log(oper1);
+var oper2 =30-6;
+console.log (oper2);
+var oper3 =48/2;
+console.log(oper3);
+var oper4 =12*2;
+console.log(oper4);
+
+// Exercise 2
+
+var op1 =6%3;
+var op2 =10%2;
+var op3 =5%2;
+
+
+console.log(op1==op3);
+console.log(op2>3);
+console.log(op3==op2);
+
+// Exercise 3
+
+//var string1 = "Test Grades as follows: \n";
+//var string2 = "student 1: 96 \n";
+//var string3= "Comment: \"none\"";
+//console.log(string1 + string2 + string3);
+
+
+// exercise 4
+
+var number= [1, 2, 3, 4 ,6 ,7 ,8 ,9 ];
+console.log(number[2]+number[4])
+
+//Exercise 5
+
+var me ={
+    name: "enrique",
+    age: "28",
+    height: "6",
+    weight:"180",
+}
+console.log(me.name);
+console.log(me.age);
+console.log(me.height);
+console.log(`hi there my name is ${me.name} I am ${me.age} years old, i am ${me.height} feet tall and I weigh ${me.weight} pounds`);
+
+// Exercise 6
+
+var num =1;
+
+while(num <10){
+    if (num % 2==0){
+        console.log(num);
+    }
+    num++
+}
+console.log
+ for(var e=0 ; e<=10; e++){
+     if(e% 2 !=0){
+         console.log(e);
+     }
+ }
+
+
